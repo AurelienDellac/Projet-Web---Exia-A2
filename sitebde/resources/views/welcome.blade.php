f8a32b8c72fded1a3d8804245889f3a2f2d7a255
CESInoe <33095171+CESInoe@users.noreply.github.com>
2019-01-22 15:57:10 +0100

    footer

--- sitebde/resources/views/welcome.blade.php
+++ sitebde/resources/views/welcome.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
         <link rel="stylesheet" type="text/css" href="css/default.css">
         <title>Laravel</title>  
 
@@ -12,37 +15,51 @@
     <body>
         <div id ="bandeau">
             <img src="/ImageTemplate/CesiBordeaux.jpg" alt="photo du CESI bordeaux" id="imgBandeau"> </div>
-            <br>
-            <div id="carre"> </a></div>
+            <div id="carre"> </div>
         </div>
 
-        <div id="footer">
-            <tr>
-                <div id="colonne1" class="colonne">
-                    <h2> CONTACT / INFORMATION <h2>
+        <div id="footer" class="flex-container">
+                <div id="contact" class="colonne">
+                    <h3> CONTACT / INFORMATION </h3>
                     <ul>
                         <li> communication@bdecesibordeaux.fr </li>
                         <li> CGU </li>
                         <li> Mentions légales </li>
                     </ul>
-                <div>
-                <div id="colonne2" class="colonne">
-                    <h2> ÉVÈNEMENTIEL </h2>
+                </div>
+                <div id="event" class="colonne">
+                    <h3> ÉVÈNEMENTIEL </h3>
                     <ul>
                         <li>Programme</li>
                         <li>Évènements passés</li>
                         <li>Boîte a idée</li>
                     </ul>
                 </div>
-                <div id="colonne3" class="colonne">
-                    <h2>Boutique</h2>
+                <div id="boutique" class="colonne">
+                    <h3>BOUTIQUE</h3>
                     <ul>
                         <li>Vêtements</li>
                         <li>Accessoires</li>
                         <li>Billeterie</li>
                         <li>Panier</li>
                     </ul>
-            </tr>
+                </div>
+                <div id="network" >
+                    <h3>NETWORK<h3>
+                    <a href = "https://www.facebook.com/bdecesibdx/">
+                        <i class="fab fa-facebook"></i>
+                    </a>
+                    <a href = "https://www.instagram.com/bde_cesi_bdx/">
+                        <i class="fab fa-instagram" ></i>
+                    </a>
+                    <a href = "https://www.cesi.fr">
+                    <img src="/ImageTemplate/CESIAlumni.png" alt="logo cesi" id="logoCesi"> </div>
+                    </a>
+                </div>
         </div>
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     </body>
 </html>
