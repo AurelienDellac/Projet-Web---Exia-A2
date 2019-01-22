0a09b2f000d2b7f822053a89d84a1586ec771c88
CESInoe <33095171+CESInoe@users.noreply.github.com>
2019-01-22 12:37:17 +0100

    test the return

--- sitebde/resources/views/welcome.blade.php
+++ sitebde/resources/views/welcome.blade.php
@@ -11,9 +11,39 @@
 
     <body>
         <div id ="bandeau">
-            <!-- <div> <img src="/ImageTemplate/CesiBordeaux.jpg" alt="photo du CESI bordeaux" id="imgBandeau"> </div> -->
+        <div> 
+            <img src="/ImageTemplate/CesiBordeaux.jpg" alt="photo du CESI bordeaux" id="imgBandeau"> </div>
             <br>
             <!-- <div id="carre"> </a></div> -->
+        </div>
+
+        <div id="footer">
+            <tr>
+                <div id="colonne1" class="colonne">
+                    <h2> CONTACT / INFORMATION <h2>
+                    <ul>
+                        <li> communication@bdecesibordeaux.fr </li>
+                        <li> CGU </li>
+                        <li> Mentions légales </li>
+                    </ul>
+                <div>
+                <div id="colonne2" class="colonne">
+                    <h2> ÉVÈNEMENTIEL </h2>
+                    <ul>
+                        <li>Programme</li>
+                        <li>Évènements passés</li>
+                        <li>Boîte a idée</li>
+                    </ul>
+                </div>
+                <div id="colonne3" class="colonne">
+                    <h2>Boutique</h2>
+                    <ul>
+                        <li>Vêtements</li>
+                        <li>Accessoires</li>
+                        <li>Billeterie</li>
+                        <li>Panier</li>
+                    </ul>
+            </tr>
         </div>
     </body>
 </html>
