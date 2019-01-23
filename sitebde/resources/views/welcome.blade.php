a5aa6af5d5d6cb3fb76329601fa984dfcf2ddab7
CESInoe <33095171+CESInoe@users.noreply.github.com>
2019-01-23 10:45:23 +0100

    less bugs with the background and the footer

--- sitebde/resources/views/welcome.blade.php
+++ sitebde/resources/views/welcome.blade.php
@@ -4,5 +4,41 @@
 @section('content')
 
     Bienvenue sur le cite du BDE du CESI Bordeaux
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Mauris congue, est id vehicula aliquet, erat magna pretium neque, quis dapibus erat felis sit amet erat. 
+        Nulla blandit lorem risus, ut pharetra mauris faucibus ac. Curabitur luctus ut lorem id venenatis. 
+        Quisque fringilla augue quis felis iaculis, nec ullamcorper massa maximus. Nunc efficitur blandit elit in cursus. 
+        Cras purus elit, faucibus vel cursus sit amet, congue quis leo. Nulla odio purus, vehicula sed tellus vel, varius scelerisque metus. 
+        Integer cursus lorem vitae bibendum rhoncus. Fusce aliquam suscipit odio a cursus. Quisque hendrerit metus purus, quis tempus dui suscipit id. 
+        In hac habitasse platea dictumst. Aliquam vitae urna et nisi pretium convallis quis id risus.
+    </p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Mauris congue, est id vehicula aliquet, erat magna pretium neque, quis dapibus erat felis sit amet erat. 
+        Nulla blandit lorem risus, ut pharetra mauris faucibus ac. Curabitur luctus ut lorem id venenatis. 
+        Quisque fringilla augue quis felis iaculis, nec ullamcorper massa maximus. Nunc efficitur blandit elit in cursus. 
+        Cras purus elit, faucibus vel cursus sit amet, congue quis leo. Nulla odio purus, vehicula sed tellus vel, varius scelerisque metus. 
+        Integer cursus lorem vitae bibendum rhoncus. Fusce aliquam suscipit odio a cursus. Quisque hendrerit metus purus, quis tempus dui suscipit id. 
+        In hac habitasse platea dictumst. Aliquam vitae urna et nisi pretium convallis quis id risus.
+    </p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Mauris congue, est id vehicula aliquet, erat magna pretium neque, quis dapibus erat felis sit amet erat. 
+        Nulla blandit lorem risus, ut pharetra mauris faucibus ac. Curabitur luctus ut lorem id venenatis. 
+        Quisque fringilla augue quis felis iaculis, nec ullamcorper massa maximus. Nunc efficitur blandit elit in cursus. 
+        Cras purus elit, faucibus vel cursus sit amet, congue quis leo. Nulla odio purus, vehicula sed tellus vel, varius scelerisque metus. 
+        Integer cursus lorem vitae bibendum rhoncus. Fusce aliquam suscipit odio a cursus. Quisque hendrerit metus purus, quis tempus dui suscipit id. 
+        In hac habitasse platea dictumst. Aliquam vitae urna et nisi pretium convallis quis id risus.
+    </p>
     
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Mauris congue, est id vehicula aliquet, erat magna pretium neque, quis dapibus erat felis sit amet erat. 
+        Nulla blandit lorem risus, ut pharetra mauris faucibus ac. Curabitur luctus ut lorem id venenatis. 
+        Quisque fringilla augue quis felis iaculis, nec ullamcorper massa maximus. Nunc efficitur blandit elit in cursus. 
+        Cras purus elit, faucibus vel cursus sit amet, congue quis leo. Nulla odio purus, vehicula sed tellus vel, varius scelerisque metus. 
+        Integer cursus lorem vitae bibendum rhoncus. Fusce aliquam suscipit odio a cursus. Quisque hendrerit metus purus, quis tempus dui suscipit id. 
+        In hac habitasse platea dictumst. Aliquam vitae urna et nisi pretium convallis quis id risus.
+    </p>
+
 @endsection
