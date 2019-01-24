ccabce31e0a952fb33a507a9703d42bb0bc33666
Orel <aurelien.dellac@gmail.com>
2019-01-24 22:10:42 +0100

    reparations

--- sitebde/routes/web.php
+++ sitebde/routes/web.php
@@ -21,7 +21,6 @@ Route::view('/panier', 'panier');
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
-Route::get('/coordinate/id', 'CenterController@index');
 
 Route::get('boutique', function () {
 
