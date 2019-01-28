fbb920b7f4f8ace2462facc98c27e1412e5c77b5
CESInoe <33095171+CESInoe@users.noreply.github.com>
2019-01-28 14:08:48 +0100

    le carousel

---
+++ sitebde/public/js/welcome.js
@@ -0,0 +1,47 @@
+var days = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+var months = ["Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Decembre"];
+
+$(function (){
+    var $contenue = $('#contenue');
+    var $evenements = $('#evenements');
+    $evenements.empty();
+    $.ajax({
+        type:'GET',
+        url: "http://10.133.129.169:3000/events/",
+        success: function(evenements) {
+            var i;
+            var max = 3;
+            if(evenements.length < 3){
+                max = evenements.length;
+            }
+
+            for(i =0; i < max; i++){
+                
+                    var date = new Date(evenements[i].date);
+
+                    if(i ==0){
+                        $contenue.append(
+                            "<div class='carousel-item active'>"+
+                                "<img src='/images/" + evenements[i].img_src +"' alt='le texte' id='monImage'>"+
+                                "<div class='carousel-caption'>"+
+                                    "<h3>" + evenements[i].title + "</h3>"+
+                                    "<p>" + days[date.getDay()] +" "+ date.getDate() +" " + months[date.getMonth()]  + "</p>"+
+                                "</div>"+
+                            "</div>"
+                        );
+                    }
+                    else{
+                        $contenue.append(
+                            "<div class='carousel-item'>"+
+                                "<img src='/images/" + evenements[i].img_src +"' alt='le texte' id='monImage'>"+
+                                "<div class='carousel-caption'>"+
+                                    "<h3>" + evenements[i].title + "</h3>"+
+                                    "<p>" + days[date.getDay()] +" "+ date.getDate() +" " + months[date.getMonth()]  + "</p>"+
+                                "</div>"+
+                            "</div>"
+                        );
+                    }
+            }
+        }
+    });
+});
